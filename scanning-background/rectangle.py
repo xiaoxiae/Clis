20bc0b0de9cf5afc4613b027d93335f9d79abcb0
xiaoxiae <tomas@slama.dev>
2021-10-01 15:19:14 +0200

    add scripts for generating photos that holds will be scanned on

---
+++ scanning-background/rectangle.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+image = Image.new("RGB", (1920, 1080))  # with default black color
+
+pixels = image.load()
+
+n = 50
+
+for x in range(image.width):
+    for y in range(image.height):
+        pixels[(x, y)] = (0, 0, 0) if ((x // n) % 2 + (y // n) % 2) % 2 == 0 else (255, 255, 255)
+
+image.save("rectangle.pdf")
