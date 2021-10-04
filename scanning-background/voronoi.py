1dd9e9f81869a86144247ea0c4c83648c3ac7110
xiaoxiae <tomas@slama.dev>
2021-10-04 11:08:35 +0200

    backgrounds: voronoi

---
+++ scanning-background/voronoi.py
@@ -0,0 +1,11 @@
+from Voronoi.voronoi import *
+
+generate(
+    path = "voronoi.pdf",
+    width = 1754,
+    height = 1240,
+    regions = 80,
+    colors = ["#000000"],
+    border_size = 35,
+    border_color = "#ffffff",
+)
