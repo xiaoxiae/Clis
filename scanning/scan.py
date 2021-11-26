2c2d31aa2b22e56426794e7edf6c39684e0d205e
xiaoxiae <tomas@slama.dev>
2021-11-26 19:21:06 +0100

    scanning: manual + automatic scanning in one script

--- scanning/scan.py
+++ scanning/scan.py
@@ -1,4 +1,4 @@
-d = """A script for creating photos using the automatic camera + Arduino setup."""
+d = """A script for creating photos automatically using a camera and optionally a turntable."""
 
 import serial
 import argparse
@@ -15,17 +15,38 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 parser = argparse.ArgumentParser(description=d)
 
-parser.add_argument("count", type=int, help="How many images to take.")
+subparsers = parser.add_subparsers(help='modes', dest='mode', required=True)
 
-parser.add_argument("--arduino-device", metavar="PATH", help="The Arduino device path (defaults to /dev/ttyACM0).", default='/dev/ttyACM0')
+auto_parser = subparsers.add_parser('automatic', help='automatic mode')
 
-parser.add_argument("-o", "--output", help="The output directory. Defaults to 'scans/'.", default='scans')
+auto_parser.add_argument(
+    "count",
+    type=int,
+    help="How many images to take.",
+)
+
+auto_parser.add_argument(
+    "--arduino-device",
+    metavar="PATH",
+    help="The Arduino device path (defaults to /dev/ttyACM0).",
+    default="/dev/ttyACM0",
+)
+
+manual_parser = subparsers.add_parser('manual', help='manual mode')
+
+parser.add_argument(
+    "-o",
+    "--output",
+    help="The output directory. Defaults to 'scans/'.",
+    default="scans",
+)
+
 
 arguments = parser.parse_args()
 
 camera = gp.Camera()
 
-print('Camera: \tconnecting to the camera...', end="", flush=True)
+print("Camera: \tconnecting to the camera...", end="", flush=True)
 while True:
     try:
         camera.init()
@@ -43,27 +64,31 @@ while True:
 print(" connected.", flush=True)
 
 
-try:
-    print('Arduino: \testablishing serial connection...', end="", flush=True)
-    ser = serial.Serial(arguments.arduino_device,
-        baudrate=9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE
-    )
+if arguments.mode == "automatic":
+    try:
+        print("Arduino: \testablishing serial connection...", end="", flush=True)
+        ser = serial.Serial(
+            arguments.arduino_device,
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+        )
 
-    if not ser.isOpen():
-        ser.open()
+        if not ser.isOpen():
+            ser.open()
 
-    # give Arduino some time
-    # important, *don't change this*
-    sleep(2)
+        # give Arduino some time
+        # important, *don't change this*
+        sleep(2)
 
-    print(" established.")
-except serial.serialutil.SerialException as e:
-    # TODO: this is probably not a good way
-    if "Errno 13" in str(e):
-        print(" failed with exit code 13, make sure the device is readable and writable.")
-        quit()
+        print(" established.")
+    except serial.serialutil.SerialException as e:
+        # TODO: this is probably not a good way
+        if "Errno 13" in str(e):
+            print(
+                " failed with exit code 13, make sure the device is readable and writable."
+            )
+            quit()
 
 
 def take_photo():
@@ -73,6 +98,7 @@ def take_photo():
     print(f" '{file_path.name}' taken.", flush=True)
 
     return file_path
+
 
 def turn_by(angle: int):
     """Turn the turntable by an angle."""
@@ -95,7 +121,9 @@ def turn_by(angle: int):
 
 
 # rotate and save the pictures at the same time
-directory = os.path.join(arguments.output, f"{datetime.now().strftime('%s')}-{arguments.count}")
+directory = os.path.join(
+    arguments.output, f"{datetime.now().strftime('%s')}-{arguments.count}"
+)
 os.mkdir(directory)
 
 angle = int(360 / arguments.count)
@@ -107,13 +135,20 @@ for i in range(arguments.count):
     camera_file = camera.file_get(photo.folder, photo.name, gp.GP_FILE_TYPE_NORMAL)
     camera_file.save(target)
 
-    if i != arguments.count - 1:
+    if i != arguments.count - 1 and arguments.mode == "automatic":
         turn_by(angle)
 
 
 for file in glob(os.path.join(directory, "*")):
-    print(f"Darktable: \tconverting photo {os.path.basename(file)}...", end="", flush=True)
-    Popen(["darktable-cli", file, os.path.join(os.path.dirname(file), ".")], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL).communicate()
+    print(
+        f"Darktable: \tconverting photo {os.path.basename(file)}...", end="", flush=True
+    )
+    Popen(
+        ["darktable-cli", file, os.path.join(os.path.dirname(file), ".")],
+        stdin=DEVNULL,
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+    ).communicate()
     print(f" done.", flush=True)
 
 camera.exit()
