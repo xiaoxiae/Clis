8420578b8e17ad5349cffd37f44284a522b1e2da
xiaoxiae <tomas@slama.dev>
2022-01-24 14:57:30 +0100

    turntable: code for 3 motor drivers

--- scanning/turntable/code/code.ino
+++ scanning/turntable/code/code.ino
@@ -1,29 +1,57 @@
 #include <AccelStepper.h>
 
-#define dirPin 2
-#define stepPin 3
-#define enablePin 4
+#define dirPin1 2
+#define stepPin1 3
+#define enablePin1 4
+#define dirPin2 5
+#define stepPin2 6
+#define enablePin2 7
+#define dirPin3 8
+#define stepPin3 9
+#define enablePin3 10
 
 #define motorInterfaceType 1
 #define stepsPerRevolution (int)(200 * 1.02)
 
-AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+#define max_speed 150.0
+#define max_acceleration 80.0
+#define speed_coefficient 0.8
+
+AccelStepper stepper1 = AccelStepper(motorInterfaceType, stepPin1, dirPin1);
+AccelStepper stepper2 = AccelStepper(motorInterfaceType, stepPin2, dirPin2);
+AccelStepper stepper3 = AccelStepper(motorInterfaceType, stepPin3, dirPin3);
 
 
 void enable_motor() {
-  digitalWrite(enablePin, LOW);
+  digitalWrite(enablePin1, LOW);
+  digitalWrite(enablePin2, LOW);
+  digitalWrite(enablePin3, LOW);
 }
 
 void disable_motor() {
-  digitalWrite(enablePin, HIGH);
+  digitalWrite(enablePin1, HIGH);
+  digitalWrite(enablePin2, HIGH);
+  digitalWrite(enablePin3, HIGH);
 }
 
 
 void setup() {
-  stepper.setMaxSpeed(150.0);
-  stepper.setAcceleration(120.0);
+  stepper1.setMaxSpeed(max_speed);
+  stepper1.setAcceleration(max_acceleration);
+  stepper1.setSpeed(max_speed * speed_coefficient);
+  
+  stepper2.setMaxSpeed(max_speed);
+  stepper2.setAcceleration(max_acceleration);
+  stepper2.setSpeed(max_speed * speed_coefficient);
+  
+  stepper3.setMaxSpeed(max_speed);
+  stepper3.setAcceleration(max_acceleration);
+  stepper3.setSpeed(max_speed * speed_coefficient);
 
-  pinMode(enablePin, OUTPUT);
+  pinMode(enablePin1, OUTPUT);
+  pinMode(enablePin2, OUTPUT);
+  pinMode(enablePin3, OUTPUT);
+
   disable_motor();
 
   Serial.begin(9600);
@@ -38,8 +66,21 @@ void loop() {
     long angle = Serial.readString().toInt();
 
     enable_motor();
-    stepper.setCurrentPosition(0);
-    stepper.runToNewPosition(angleToRevolutions(angle));
+    stepper1.setCurrentPosition(0);
+    stepper1.moveTo(angleToRevolutions(angle));
+    
+    stepper2.setCurrentPosition(0);
+    stepper2.moveTo(angleToRevolutions(angle));
+    
+    stepper3.setCurrentPosition(0);
+    stepper3.moveTo(angleToRevolutions(angle));
+
+    while (stepper1.distanceToGo() > 0 && stepper2.distanceToGo() > 0 && stepper3.distanceToGo() > 0) {
+      stepper1.run();
+      stepper2.run();
+      stepper3.run();
+    }
+    
     disable_motor();
 
     Serial.println(angle, DEC);
