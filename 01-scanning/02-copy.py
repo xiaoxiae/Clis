b802907f87e8ea2dd052f8b720c30c6cc36af7ed
xiaoxiae <tomas@slama.dev>
2022-02-21 16:46:25 +0100

    scanning: copy: add camera mode
    Quite useful for development as you can chain 01, 02 and 03 together in a single call.

--- 01-scanning/02-copy.py
+++ 01-scanning/02-copy.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import tempfile
 import shutil
+import gphoto2 as gp
 
 from subprocess import Popen, PIPE
 from glob import glob
@@ -14,26 +15,39 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 parser = argparse.ArgumentParser(description=d)
 
-parser.add_argument("--sd-path", help=f"The path to the SD card. Defaults to '{SD_PATH}'.", default=SD_PATH)
+save_path = os.path.join(CAMERA_SAVE_PATH, IMAGE_SAVE_PATH.lstrip("/"))
+
+subparsers = parser.add_subparsers(help="copy mode", dest="mode", required=True)
+
+camera_parser = subparsers.add_parser("camera", help="from camera")
+
+sd_parser = subparsers.add_parser("card", help="from SD card")
 
 arguments = parser.parse_args()
 
-if not os.geteuid() == 0:
-    print("Copy: \tmust be run as root.")
-    quit()
+camera = gp.Camera()
 
-mount_folder = tempfile.TemporaryDirectory()
+if arguments.mode == "camera":
+    def connect_to_camera():
+        """Connect to the camera."""
+        print("Copy: \tConnecting to the camera...", end="", flush=True)
+        while True:
+            try:
+                camera.init()
+            except gp.GPhoto2Error as ex:
+                # this is not too pretty, but works...
+                try:
+                    if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
+                        sleep(2)
+                        continue
+                    raise
+                except KeyboardInterrupt:
+                    print(" interrupted by the user, quitting.", flush=True)
+                    quit()
+            break
+        print(" connected.", flush=True)
 
-def unmount():
-    print(f"Copy: \tunmounting {SD_PATH}...", flush=True, end=" ")
-    Popen(["sudo", "umount", mount_folder.name]).communicate()
-    print("unmounted.")
-
-try:
-    # mount the SD path 
-    print(f"Copy: \tmounting {SD_PATH}...", flush=True, end=" ")
-    Popen(["sudo", "mount", SD_PATH, mount_folder.name]).communicate()
-    print("mounted.")
+    connect_to_camera()
 
     for folder in glob("*"):
         for file in [f for f in glob(os.path.join(folder, "**", "images.txt"))]:
@@ -43,24 +57,57 @@ try:
             with open(file) as f:
                 photos = f.read().splitlines()
                 for photo in photos:
-                    photo_src = os.path.join(mount_folder.name, IMAGE_SAVE_PATH.lstrip("/"), photo)
-                    photo_dest = os.path.join(dest_folder, photo)
-
                     print(f"Copy: \tcopying '{photo}':", flush=True, end="")
-                    shutil.copyfile(photo_src, photo_dest)
+                    gp_file = camera.file_get(save_path, photo, gp.GP_FILE_TYPE_NORMAL)
+                    result_path = os.path.join(dest_folder, photo)
+                    gp_file.save(result_path)
                     print(" copied.")
 
             os.remove(file)
+else:
+    if not os.geteuid() == 0:
+        print("Copy: \tmust be run as root.")
+        quit()
 
-    unmount()
+    mount_folder = tempfile.TemporaryDirectory()
 
-    mount_folder.cleanup()
+    def unmount():
+        print(f"Copy: \tunmounting {SD_PATH}...", flush=True, end=" ")
+        Popen(["sudo", "umount", mount_folder.name]).communicate()
+        print("unmounted.")
 
-except Exception as e:
-    print(f"Copy: \tA fatal error occurred while copying the files: {e}")
+    try:
+        # mount the SD path
+        print(f"Copy: \tmounting {SD_PATH}...", flush=True, end=" ")
+        Popen(["sudo", "mount", SD_PATH, mount_folder.name]).communicate()
+        print("mounted.")
 
-    unmount()
+        for folder in glob("*"):
+            for file in [f for f in glob(os.path.join(folder, "**", "images.txt"))]:
+                dest_folder = os.path.dirname(file)
 
-username = Popen(["logname"], stdout=PIPE).communicate()[0].decode().strip()
+                print(f"Copy: \tcopying images from '{file}':")
+                with open(file) as f:
+                    photos = f.read().splitlines()
+                    for photo in photos:
+                        photo_src = os.path.join(mount_folder.name, IMAGE_SAVE_PATH.lstrip("/"), photo)
+                        photo_dest = os.path.join(dest_folder, photo)
 
-Popen(["sudo", "chown", "-R", f"{username}:{username}", SCAN_PATH]).communicate()
+                        print(f"Copy: \tcopying '{photo}':", flush=True, end="")
+                        shutil.copyfile(photo_src, photo_dest)
+                        print(" copied.")
+
+                os.remove(file)
+
+        unmount()
+
+        mount_folder.cleanup()
+
+    except Exception as e:
+        print(f"Copy: \tA fatal error occurred while copying the files: {e}")
+
+        unmount()
+
+    username = Popen(["logname"], stdout=PIPE).communicate()[0].decode().strip()
+
+    Popen(["sudo", "chown", "-R", f"{username}:{username}", SCAN_PATH]).communicate()
