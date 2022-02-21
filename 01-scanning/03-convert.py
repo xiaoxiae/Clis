bcda9d20b887f17398d9bd7d4f13099c8c93cfd9
xiaoxiae <tomas@slama.dev>
2022-02-21 16:45:37 +0100

    scanning: remove the original while converting

--- 01-scanning/03-convert.py
+++ 01-scanning/03-convert.py
@@ -13,14 +13,16 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 parser = argparse.ArgumentParser(description=d)
 
-parser.add_argument("-i", "--input", help=f"The input folder. Defaults to '{SCAN_PATH}'.", default=SCAN_PATH)
-parser.add_argument("-f", "--format", help=f"The image format. Defaults to '{IMAGE_EXTENSION}'.", default=IMAGE_EXTENSION)
-
 arguments = parser.parse_args()
 
 
-for folder in glob(os.path.join(arguments.input, "*")):
-    for file in [f for f in glob(os.path.join(folder, "*")) if f.lower().endswith(arguments.format)]:
+for folder in glob(os.path.join(SCAN_PATH, "*")):
+    for file in glob(os.path.join(folder, "*")):
+        if not file.lower().endswith(IMAGE_EXTENSION):
+            continue
+
         print(f"Darktable: \tconverting file {os.path.basename(file)}...", end="", flush=True)
         Popen(["darktable-cli", file, os.path.join(os.path.dirname(file), ".")], stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL).communicate()
+        print(f" removing the original...", end="", flush=True)
+        os.remove(file)
         print(f" done.", flush=True)
