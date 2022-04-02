43688822dcef145f928f3db16d19ebb98987f48f
xiaoxiae <tomas@slama.dev>
2022-04-02 17:41:16 +0200

    scanning: number of holds to scan

--- 01-scanning/01-scan.py
+++ 01-scanning/01-scan.py
@@ -17,6 +17,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument(
+    "-n",
+    "--number",
+    help="The number of holds to scan; defaults to 1, -1 means infinite.",
+    default=1,
+    type=int,
+)
+
 subparsers = parser.add_subparsers(help="scanning mode", dest="mode", required=True)
 
 auto_parser = subparsers.add_parser("automatic", help="automatic mode")
@@ -24,7 +32,7 @@ auto_parser = subparsers.add_parser("automatic", help="automatic mode")
 auto_parser.add_argument(
     "count",
     type=int,
-    help="How many images to take.",
+    help="How many images of a hold to take.",
 )
 
 auto_parser.add_argument(
@@ -36,13 +44,6 @@ auto_parser.add_argument(
 
 
 manual_parser = subparsers.add_parser("manual", help="manual mode")
-
-parser.add_argument(
-    "-o",
-    "--output",
-    help="The output directory. Defaults to 'scans/'.",
-    default="scans",
-)
 
 
 arguments = parser.parse_args()
@@ -92,7 +93,9 @@ if automatic:
     except serial.serialutil.SerialException as e:
         # TODO: this is probably not a good way
         if "Errno 13" in str(e):
-            printer.end(f"failed with exit code 13, make sure the device ({ARDUINO_PATH}) is readable and writable.")
+            printer.end(
+                f"failed with exit code 13, make sure the device ({ARDUINO_PATH}) is readable and writable."
+            )
             sys.exit(1)
 
 
@@ -122,79 +125,92 @@ else:
     # a bit of a hack to take a lot of pictures manually
     arguments.count = 999
 
-try:
-    # the photos before the start of the shooting
-    initial_photos = list(camera.folder_list_files(camera_path))
-except gphoto2.GPhoto2Error:
-    printer.full(f"the path '{camera_path}' not found on the camera, make sure that it is correct and that the camera is turned on.")
-    sys.exit(1)
 
-
-for i in range(arguments.count):
-    printer.begin("taking a photo: focusing")
-    try:
-        while True:
-            try:
-                focus()
-                break
-            except gp.GPhoto2Error:
-                printer.mid("unable to focus, retrying")
-    except KeyboardInterrupt:
-        printer.full("\nInterrupted by the user, taking no more pictures.")
+n = 0
+while True:
+    # called here to work for 0 photos
+    if n == arguments.number:
         break
 
+    if n != 0:
+        printer.begin("waiting for enter to scan the next hold")
+        input()
+
+    n += 1
+
     try:
-        printer.mid("capturing")
-        camera.trigger_capture()
-        printer.end("done.")
+        # the photos before the start of the shooting
+        initial_photos = list(camera.folder_list_files(camera_path))
+    except gphoto2.GPhoto2Error:
+        printer.full(
+            f"the path '{camera_path}' not found on the camera, make sure that it is correct and that the camera is turned on."
+        )
+        sys.exit(1)
 
-        # don't move on the very last photo (or if we're not in automatic)
-        if automatic:
-            if i != arguments.count - 1:
-                turn_by(angle)
-        else:
-            printer.full("press enter when the hold has been turned.", end="")
-            input()
-    except gp.GPhoto2Error as e:
-        printer.end("camera error! Attempt to reconnect? (y/n): ", end="")
-        answer = input().strip().lower()
 
-        if answer == "y":
-            connect_to_camera()
-        else:
+    for i in range(arguments.count):
+        printer.begin("taking a photo: focusing")
+        try:
+            while True:
+                try:
+                    focus()
+                    break
+                except gp.GPhoto2Error:
+                    printer.mid("unable to focus, retrying")
+        except KeyboardInterrupt:
+            printer.full("\nInterrupted by the user, taking no more pictures.")
             break
-    except KeyboardInterrupt:
-        printer.full("Interrupted by the user, taking no more pictures.")
-        break
 
-camera.exit()
+        try:
+            printer.mid("capturing")
+            camera.trigger_capture()
+            printer.end("done.")
 
-# important, *don't change this*, otherwise camera.folder_list_files won't work
-printer.begin("waiting for camera to save changes")
-sleep(1)
-printer.end("done.")
+            # don't move on the very last photo (or if we're not in automatic)
+            if automatic:
+                if i != arguments.count - 1:
+                    turn_by(angle)
+            else:
+                printer.full("press enter when the hold has been turned.", end="")
+                input()
+        except gp.GPhoto2Error as e:
+            printer.end("camera error! Attempt to reconnect? (y/n): ", end="")
+            answer = input().strip().lower()
 
-# the photos after the shooting
-current_photos = list(camera.folder_list_files(camera_path))
+            if answer == "y":
+                connect_to_camera()
+            else:
+                break
+        except KeyboardInterrupt:
+            printer.full("Interrupted by the user, taking no more pictures.")
+            break
 
-count = len(current_photos) - len(initial_photos)
+    camera.exit()
 
-# don't create any folders or files if no photos were taken
-if count == 0:
-    printer.full("No photos were taken, not saving anything!")
-    sys.exit(1)
+    # important, *don't change this*, otherwise camera.folder_list_files won't work
+    printer.begin("waiting for camera to save changes")
+    sleep(1)
+    printer.end("done.")
 
-directory = os.path.join(
-    arguments.output, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-)
-os.mkdir(directory)
+    # the photos after the shooting
+    current_photos = list(camera.folder_list_files(camera_path))
 
-# save the names of the shot photos to download them later
-with open(os.path.join(directory, "images.txt"), "w") as file:
-    for f in sorted(current_photos):
-        if f not in initial_photos:
-            name, _ = f
+    count = len(current_photos) - len(initial_photos)
 
-            file.write(f"{name}\n")
+    # don't create any folders or files if no photos were taken
+    if count == 0:
+        printer.full("No photos were taken, not saving anything!")
+        sys.exit(1)
 
-printer.full(f"Saved {count} photos.")
+    directory = os.path.join(SCAN_PATH, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+    os.mkdir(directory)
+
+    # save the names of the shot photos to download them later
+    with open(os.path.join(directory, "images.txt"), "w") as file:
+        for f in sorted(current_photos):
+            if f not in initial_photos:
+                name, _ = f
+
+                file.write(f"{name}\n")
+
+    printer.full(f"Saved {count} photos.")
